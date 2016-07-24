20dc1498225a186dae52323b3232c38004ed7392
Rolf Erik Lekang <me@rolflekang.com>
2016-07-24 21:07:20 +0200

    refactor: Use jscodeshift cli helper to evaluate args

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-console */
-const Runner = require('jscodeshift/dist/Runner');
-const chalk = require('chalk');
-const { omit } = require('lodash');
-const { resolve } = require('path');
+const resolve = require('path').resolve;
+const cli = require('jscodeshift').cli;
 
-module.exports = args => {
-  console.log(chalk.green(chalk.bold(`Transforming using '${args.transform}' ✌`)));
-  const options = Object.assign({ quote: 'single' }, omit(args, ['_', 'transform', 'path']));
-  Runner.run(resolve(__dirname, `./${args.transform}/index.js`), args.path, options);
-};
+const pkg = require('./package.json');
+
+cli({
+  name: 'rcm',
+  packageVersion: pkg.info,
+  options: { quote: 'single' },
+  resolve(name) {
+    return resolve(__dirname, `./${name}/index.js`);
+  },
+});
